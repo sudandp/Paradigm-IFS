67156e66bad10c736dc3ac80b5db575134535bc8
Sudhan <sudhan@paradigmfms.com>
2025-11-24 17:12:57 +0530

    commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e8e5feucf0g"
+    "revision": "0.g7pqocbeck"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
